bff215054ca3733d953e597fa4ef47ad2f7dfe91
Jose Salvador Soto R <salvador.soto.qro@gmail.com>
2023-11-14 10:33:35 -0600

    Final working code

--- index.js
+++ index.js
@@ -1,42 +1,50 @@
 import puppeteer from "puppeteer";
-import fs from 'fs';
 
 async function getTasksFromTrello() {
-  // const browser = await puppeteer.launch({ headless: "new" });
+  //const browser = await puppeteer.launch({ headless: "new" });
 
+  //Start browser
   const browser = await puppeteer.launch({
     slowMo: 200,
     headless: false,
-    args: [
-      `--disable-extensions-except=${EXTENSION_PATH}`,
-      `--load-extension=${EXTENSION_PATH}`
-    ]
   });
+  //Create a ew page
   const page = await browser.newPage();
+  //Set timeout to limitless
   await page.setDefaultNavigationTimeout(0);
-  await page.goto("https://trello.com/b/QvHVksDa/personal-work-goals");
+  //Go to the trello page and wait to load
+  await page.goto("https://trello.com/b/QvHVksDa/personal-work-goals", {
+    waitUntil: "load",
+    timeout: 0,
+  });
 
+  //Evaluate page to get la list column
   const data = await page.evaluate(() => {
+    //Get all the trello columns
     const list = document.querySelectorAll('[data-testid="list-wrapper"]');
     var tasks = {};
+    //Loop through the trello colimns and find the tasks
     for (i = 0; i < list.length; i++) {
       taskItems = {};
       let title = list[i].querySelector("h2").innerHTML;
       const items = list[i].querySelectorAll('li[data-testid="list-card"] a');
 
+      //Loop to get the tasks
       for (j = 0; j < items.length; j++) {
         let itemText = items[j].text;
         taskItems[itemText] = itemText;
       }
-
+      //Set each item object to set data
       tasks[title] = {
         title,
         taskItems,
       };
     }
 
+    //Get the description
     let description = document.querySelector("p").innerText;
 
+    //return the object with data from trello
     return {
       list: list.length,
       description,
@@ -44,62 +52,86 @@ async function getTasksFromTrello() {
     };
   });
 
-  await page.goto("https://todoist.com/auth/login", {
+  //Create a new page
+  const page2 = await browser.newPage();
+  //Go to the login page of todoist site
+  await page2.goto("https://todoist.com/auth/login", {
     waitUntil: "load",
     timeout: 0,
   });
 
-    //type="email"
-    await page.type("input[type=email]", "salvador.soto.qro@gmail.com");
-    //type="password"
-    await page.type("input[type=password]", "Pruebatest,2023");
-    await Promise.all([
-      page.click("button[type=submit]"),
-      //cookies = await page.cookies(),
-      page.waitForNavigation({ waitUntil: 'networkidle0' }),
-  
-    ]);
+  //Set the username
+  await page2.type("input[type=email]", "salvador.soto.qro@gmail.com");
+  //Set the password
+  await page2.type("input[type=password]", "Pruebatest,2023");
+  await Promise.all([
+    //Login and wait for login success
+    page2.click("button[type=submit]"),
+    page2.waitForNavigation({ waitUntil: "networkidle0" }),
+  ]);
+  //Get the tasks from trello data
+  let tasksToSearch = data["tasks"];
+  let counter = 1;
 
-    let tasksToSearch=data["tasks"];
-    let counter=1;
-    await Object.keys(tasksToSearch).forEach((key)=>{
-      const titleToAdd=tasksToSearch[key]['title'];
-      let taskItemsToSearch=tasksToSearch[key]['taskItems'];
-      let itemToADD='';
-      Object.keys(taskItemsToSearch).forEach((key)=>{
-        itemToADD=taskItemsToSearch[key];
-        return true;
-      });
-  
-      if(counter==5){
-        return true;
-      }else{
-        setTaskTodoist(page,titleToAdd,itemToADD);
-        itemToADD ++
-      }
-    });
+  //Loop through the trello data titles
+  for (const key of Object.keys(tasksToSearch)) {
+    let titleToAdd = String(tasksToSearch[key]["title"]);
+    let taskItemsToSearch = tasksToSearch[key]["taskItems"];
+    let itemToADD = "";
 
+    //Loop through the trello tasks
+    for (const key2 of Object.keys(taskItemsToSearch)) {
+      console.log(taskItemsToSearch[key2]);
+      itemToADD = String(taskItemsToSearch[key2]);
+      break;
+    }
+
+    //Check for 5 tasks only with the counter
+    if (counter == 6) {
+      return false;
+    } else {
+      console.log("before todolist" + titleToAdd + "=>" + itemToADD);
+      //Call the create task in todoist function
+      await setTaskTodoist(page2, titleToAdd, itemToADD);
+
+      //increase the counter
+      counter++;
+    }
+  }
+  //await new Promise((resolve) => setTimeout(resolve, 1500));
   await browser.close();
 }
 
-
-
-async function setTaskTodoist(page,title,task){
+//Function to save the tasks in todoist
+async function setTaskTodoist(page, title, task) {
+  //Click on add task button
   await page.click('a[aria-label="Añadir tarea"]');
-  page.waitForNavigation({ waitUntil: 'networkidle0' });
+  page.waitForNavigation({ waitUntil: "networkidle0" });
 
-  await page.evaluate(() => {
-    let todoTitle= document.querySelector('.task_editor__input_fields [data-placeholder="Nombre de la tarea"]');
-    todoTitle.innerHTML=title;
+  //Enter evcaluate on page
+  await page.evaluate(
+    (title, task) => {
+      //Set the task title
+      let todoTitle = document.querySelector(
+        '.task_editor__input_fields [data-placeholder="Nombre de la tarea"]'
+      );
+      todoTitle.innerHTML = title;
 
-    let todoDescription= document.querySelector('.task_editor__input_fields [aria-label="Descripción"]');
-    todoDescription.innerHTML=task;
+      //Set the task description
+      let todoDescription = document.querySelector(
+        '.task_editor__input_fields [aria-label="Descripción"]'
+      );
+      todoDescription.innerHTML = task;
+    },
+    title,
+    task
+  );
 
-  });
-  
+  //Submit butto click to add task
   await page.click('button[data-testid="task-editor-submit-button"]');
-  page.waitForNavigation({ waitUntil: 'networkidle0' });
-await new Promise((resolve) => setTimeout(resolve, 1500));
+
+  //Wait to finish navigation
+  page.waitForNavigation({ waitUntil: "networkidle0" });
 }
 
 getTasksFromTrello();
